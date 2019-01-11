58efa5fb8d548dc9c59d2fdaaa93b347a1976dc1
eugen neagoe <eneagoe@gmail.com>
2019-01-11 15:40:35 +0200

    Add C++ solution for https://codeforces.com/problemset/problem/617/A

---
+++ cpp/617A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    if (n % 5)
+        cout << n / 5 + 1 << endl;
+    else
+        cout << n / 5 << endl;
+
+    return 0;
+}
